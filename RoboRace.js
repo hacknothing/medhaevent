4be1a38fb33acf903f9d1cb2ee6072c68de8c42b
narendramongam <mongamnarendra@gmail.com>
2024-07-26 20:11:42 +0530

    add

---
+++ RoboRace.js
@@ -0,0 +1,12 @@
+var arr=["pptPresentation.jpg","Ideathon.webp","robo-race4-800.png"]
+
+var index =0;
+function LeftSide()
+{
+    document.getElementById("img-slide").src=arr[index];
+}
+
+function RightSide()
+{
+    document.getElementById("img-slide").style.backgroundImage;
+}
